2e568bd406993182a47c5197185a47a56771cf67
Jana <janarthani.d2023csbs@sece.ac.in>
2026-06-15 20:04:34 +0530

    Add day1 question6 Answer

---
+++ question6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class question6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("Hexadecimal number is : " + Integer.toHexString(n).toUpperCase());
+    }
+}
