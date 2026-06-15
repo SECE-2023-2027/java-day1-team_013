3e3de9c453565a9bc40498031d6bacdf8dc261e4
Jana <janarthani.d2023csbs@sece.ac.in>
2026-06-15 19:47:58 +0530

    Add day1 question5 Answer

---
+++ question5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class question5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("Binary number is: " + Integer.toBinaryString(n));
+    }
+}
