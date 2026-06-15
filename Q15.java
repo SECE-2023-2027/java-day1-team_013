1118363a04112f3a65c82e7f125952487cbc9cc4
DharanyaS2005 <dharanya.s2023cse@sece.ac.in>
2026-06-15 22:08:39 +0530

    q15

---
+++ Q15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        boolean result = (a % 10 == b % 10) ||
+                         (a % 10 == c % 10) ||
+                         (b % 10 == c % 10);
+        System.out.println("The result is: " + result);
+        sc.close();
+    }
+}
